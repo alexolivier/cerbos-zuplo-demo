35e722106a6bc8c212238d94309cb09a33bc42ee
Alex Olivier <alex@alexolivier.me>
2024-08-15 13:07:10 +0100

    test
    Signed-off-by: Alex Olivier <alex@alexolivier.me>

--- modules/cerbos.ts
+++ modules/cerbos.ts
@@ -11,7 +11,30 @@ export default async function policy(
   options: MyPolicyOptionsType,
   policyName: string
 ) {
-  const cerbos = new HTTP("");
+  const cerbos = new HTTP("https://cerbos-zuplo-demo-piy6w63peq-uc.a.run.app");
+
+  const checkPermissions = await cerbos.checkResource({
+    principal: {
+      id: "some-id",
+      roles: ["user"],
+      attr: {},
+    },
+    resource: {
+      kind: "route",
+      id: context.requestId,
+      attr: {
+        method: request.method,
+        path: request.url,
+        params: JSON.parse(JSON.stringify(request.params)),
+        headers: JSON.parse(JSON.stringify(request.headers)),
+      },
+    },
+    actions: [request.method],
+  });
+
+  if (!checkPermissions.isAllowed(request.method)) {
+    return new Response(`Unauthorized`, { status: 401 });
+  }
 
   return request;
 }
