024bf7562013083e958ed651bbaa3383c77aa0f2
Alex Olivier <alex@alexolivier.me>
2024-08-15 13:08:44 +0100

    fix
    Signed-off-by: Alex Olivier <alex@alexolivier.me>

--- modules/cerbos.ts
+++ modules/cerbos.ts
@@ -1,5 +1,5 @@
 import { ZuploContext, ZuploRequest } from "@zuplo/runtime";
-import { HTTP } from "./third-party/@cerbos/http";
+import HTTP from "./third-party/@cerbos/http";
 
 type MyPolicyOptionsType = {
   myOption: any;
@@ -11,7 +11,9 @@ export default async function policy(
   options: MyPolicyOptionsType,
   policyName: string
 ) {
-  const cerbos = new HTTP("https://cerbos-zuplo-demo-piy6w63peq-uc.a.run.app");
+  const cerbos = new HTTP.HTTP(
+    "https://cerbos-zuplo-demo-piy6w63peq-uc.a.run.app"
+  );
 
   const checkPermissions = await cerbos.checkResource({
     principal: {
