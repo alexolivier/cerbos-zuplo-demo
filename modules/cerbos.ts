8da95320a8c4b1f38f9dfadcfea9792b7525b86f
Alex Olivier <alex@alexolivier.me>
2024-09-04 17:17:46 +0100

    update module
    Signed-off-by: Alex Olivier <alex@alexolivier.me>

--- modules/cerbos.ts
+++ modules/cerbos.ts
@@ -5,6 +5,8 @@ type CerbosOptionsType = {
   pdpUrl: string;
   defaultRole?: string;
   roleClaimName?: string;
+  sendAuthorizationHeader?: boolean;
+  includePolicyOutputs?: boolean;
 };
 
 export default async function policy(
@@ -13,10 +15,15 @@ export default async function policy(
   options: CerbosOptionsType,
   policyName: string
 ) {
-  const { defaultRole = "ANONYMOUS", pdpUrl, roleClaimName } = options;
-  const tokenHeader = request.headers.get("Authorization");
+  const {
+    defaultRole = "ANONYMOUS",
+    pdpUrl,
+    roleClaimName,
+    sendAuthorizationHeader,
+    includePolicyOutputs,
+  } = options;
 
-  if (!request.user || !tokenHeader) {
+  if (!request.user) {
     context.log.error(
       "Not authenticated - an authentication policy is required"
     );
@@ -26,64 +33,71 @@ export default async function policy(
   const cerbos = new Cerbos(pdpUrl);
   const url = new URL(request.url);
 
-  try {
-    const tokenString = tokenHeader.split(" ")[1];
-
-    let roles = [defaultRole];
-    if (roleClaimName) {
-      const roleClaim = request.user.data[roleClaimName];
-      if (roleClaim) {
-        if (Array.isArray(roleClaim)) {
-          roles = roleClaim;
-        } else {
-          roles = [JSON.stringify(roleClaim)];
-        }
+  let roles = [defaultRole];
+  if (roleClaimName) {
+    const roleClaim = request.user.data[roleClaimName];
+    if (roleClaim) {
+      if (Array.isArray(roleClaim)) {
+        roles = roleClaim;
+      } else {
+        roles = [JSON.stringify(roleClaim)];
       }
     }
+  }
 
-    const payload = {
-      requestId: context.requestId,
-      principal: {
-        id: request.user.sub,
-        roles,
-        attr: {},
+  const payload = {
+    requestId: context.requestId,
+    principal: {
+      id: request.user.sub,
+      roles,
+      attr: {},
+    },
+    resource: {
+      kind: "route",
+      id: url.pathname,
+      attr: {
+        protocol: url.protocol,
+        method: request.method,
+        host: url.host,
+        pathname: url.pathname,
+        search: url.search,
+        policyName,
       },
-      resource: {
-        kind: "route",
-        id: url.pathname,
-        attr: {
-          protocol: url.protocol,
-          method: request.method,
-          host: url.host,
-          pathname: url.pathname,
-          search: url.search,
-          policyName,
-        },
-      },
-      action: request.method,
-      auxData: {
+    },
+    actions: [request.method],
+  };
+
+  if (sendAuthorizationHeader) {
+    const tokenHeader = request.headers.get("Authorization");
+    if (tokenHeader) {
+      const tokenString = tokenHeader.split(" ")[1];
+      payload["auxData"] = {
         jwt: {
           token: tokenString,
         },
-      },
-    };
+      };
+    }
+  }
 
-    context.log.debug(`Cerbos request: ${JSON.stringify(payload)}`);
+  context.log.debug(`Cerbos request: ${JSON.stringify(payload)}`);
 
-    const allowed = await cerbos.isAllowed(payload);
+  const result = await cerbos.checkResource(payload);
 
-    if (!allowed) {
-      context.log.error(
-        `The user '${request.user.sub}' with roles ${roles.join(
-          ", "
-        )} is not authorized by Cerbos`
-      );
-      return HttpProblems.forbidden(request, context);
+  if (!result.isAllowed(request.method)) {
+    context.log.error(
+      `The user '${request.user.sub}' with roles ${roles.join(
+        ", "
+      )} is not authorized by Cerbos`
+    );
+
+    if (includePolicyOutputs) {
+      return HttpProblems.forbidden(request, context, {
+        outputs: includePolicyOutputs,
+      });
     }
 
-    return request;
-  } catch (e) {
-    context.log.error(`Cerbos error`, e);
     return HttpProblems.forbidden(request, context);
   }
+
+  return request;
 }
